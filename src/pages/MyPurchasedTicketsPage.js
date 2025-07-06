6bffbd0b7efa9283d3df1245f7117c654f5ec4d8
andres050588 <andres050588@gmail.com>
2025-07-06 23:29:42 +0200

    escluso button show all

--- src/pages/MyPurchasedTicketsPage.js
+++ src/pages/MyPurchasedTicketsPage.js
@@ -2,5 +2,5 @@ import React from "react"
 import TicketList from "../components/TicketList"
 
 export default function MyPurchasedTicketsPage() {
-    return <TicketList title="Biglietti Acquistati" limit={6} apiEndpoint="/orders/purchased" ticketType="purchased" showSeeAllButton={true} />
+    return <TicketList title="Biglietti Acquistati" apiEndpoint="/orders/purchased" ticketType="purchased" />
 }
