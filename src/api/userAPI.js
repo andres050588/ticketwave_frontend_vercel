6501f72f2f510e36bde1ce9e65cad5f453f34c6e
andres050588 <andres050588@gmail.com>
2025-07-06 22:06:17 +0200

    config.headers["Cache-Control"] = "no-cache"

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -9,6 +9,7 @@ userAPI.interceptors.request.use(
     config => {
         const token = localStorage.getItem("token")
         if (token) config.headers.Authorization = `Bearer ${token}`
+        config.headers["Cache-Control"] = "no-cache"
         return config
     },
     error => Promise.reject(error)
